1f38d6e397a7b400e1287c2ca112e06a41a338e8
liseokpo <samaniegoeliseo@gmail.com>
2021-01-04 19:27:58 -0300

    fix proy funca

--- src/main/java/com/example/proyecto_x/api/HorarioRepository.java
+++ src/main/java/com/example/proyecto_x/api/HorarioRepository.java
@@ -1,7 +1,9 @@
 package com.example.proyecto_x.api;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HorarioRepository extends MongoRepository<Horario, Integer>{
     
 }
